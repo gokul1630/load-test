99e570aa336e24458dcb7ce7db599a8ed580d0cd
gokul1630 <gokulgp3167@gmail.com>
2023-08-02 12:14:32 +0530

    init

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+app.get('/', (req, res)=>{
+	res.send('express server')
+})
+
+app.get('/load', (req, res)=>{
+	let count = 0
+	for (let index = 0; index < 1e+10; index++) {
+		count += 1
+	}
+	res.json({ count })
+})
+
+app.listen(8081)
